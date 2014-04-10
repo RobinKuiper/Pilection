4eebfc9d075a69aa8192e57a37af7b6230dd6114
Robin Kuiper <robingjkuiper@gmail.com>
2014-04-10 14:46:48 +0200

    Seeder

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -29,7 +29,7 @@
 			    <!-- Collect the nav links, forms, and other content for toggling -->
 			    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			      <ul class="nav navbar-nav">
-			        <li class="active">{{ Link_to('/', 'Systems') }}</li>
+			        <li class="active">{{ Link_to('systems', 'Systems') }}</li>
                                 <li>{{ Link_to('scripts', 'Scripts') }}</li>
                                 <li>{{ Link_to('projects', 'Projects') }}</li>
 			        <li>{{ Link_to('about', 'About') }}</li>

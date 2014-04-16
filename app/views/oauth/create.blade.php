26db2dbaa6a3df203cb58692f08c8c6b6defca3d
Robin Kuiper <robingjkuiper@gmail.com>
2014-04-17 00:16:40 +0200

    Bugfix

--- app/views/oauth/create.blade.php
+++ app/views/oauth/create.blade.php
@@ -1,35 +1,3 @@
-@extends('layouts.master')
-
-@section('breadcrumbs')
-{{ Breadcrumbs::render('register') }}
-@stop
-
-@section('content')
-
 @foreach($userProfile as $key => $value)
 <li>{{ $key }} : {{ $value }}</li>
 @endforeach
-
-{{ Form::open(['route' => 'oauth.store', 'class' => 'form-signup', 'role' => 'form']) }}
-<h2 class="form-signup-heading">Please Register</h2>
-
-<div class="form-group">
-    {{ Form::text('username', ['class'=>'form-control', 'placeholder'=>'User Name']) }}
-    {{ $errors->first('username') }}
-</div>
-
-<div class="form-group">
-    {{ Form::password('password', ['class'=>'form-control', 'placeholder'=>'Password']) }}
-    {{ $errors->first('password') }}
-</div>
-
-<div class="form-group">
-    {{ Form::password('password_confirmation', ['class'=>'form-control', 'placeholder'=>'Confirm Password']) }}
-    {{ $errors->first('password_confirmation') }}
-</div>
-
-{{ Form::submit('Register', array('class'=>'btn btn-large btn-primary btn-block'))}}
-
-{{ Form::close() }}
-
-@stop

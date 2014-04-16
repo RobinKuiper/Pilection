05dd4a1d30e722b80ee8af6f7603a099f7beb73d
Robin Kuiper <robingjkuiper@gmail.com>
2014-04-17 00:02:28 +0200

    oAuth fix

--- app/controllers/oAuthController.php
+++ app/controllers/oAuthController.php
@@ -47,7 +47,7 @@ class oAuthController extends \BaseController
             Session::flash('error_msg', $e->getMessage());
             return Redirect::to('/login');
         }
-
+return $userProfile;
         return View::make('oauth.create', ['userprofile' => $userProfile])
     }
 
@@ -61,7 +61,7 @@ class oAuthController extends \BaseController
 
         $userProfile->email = 'test@test.nl';
         $userProfile->emailVerfied = 'test@test.nl';
-        
+
         if (isset($userProfile->username))
             $username = strlen($userProfile->username) > 0 ? $userProfile->username : "";
 

b4dc4540e138eb8db0e3a74a7ccab73fec1cb7a6
Robin Kuiper <robingjkuiper@gmail.com>
2014-04-21 15:23:31 +0200

    Changed welcome mail

--- app/views/emails/auth/validation.blade.php
+++ app/views/emails/auth/validation.blade.php
@@ -2,12 +2,120 @@
 <html lang="en-US">
 <head>
     <meta charset="utf-8">
+    <title>Welcome to Pilection</title>
 </head>
 <body>
-<h2>Email validation</h2>
+<center>
+    <table width="600" background="#FFFFFF" style="text-align:left;" cellpadding="0" cellspacing="0">
+        <tr>
+            <td height="18" width="31" style="border-bottom:1px solid #e4e4e4;">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+            <td height="18" width="131">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+            <td height="18" width="466" style="border-bottom:1px solid #e4e4e4;">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+        </tr>
+        <tr>
+            <td height="2" width="31" style="border-bottom:1px solid #e4e4e4;">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+            <td height="2" width="131">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+            <td height="2" width="466" style="border-bottom:1px solid #e4e4e4;">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+        </tr>
+        <!--GREEN STRIPE-->
+        <tr>
+            <td background="images/greenback.gif" width="31" bgcolor="#45a853" style="border-top:1px solid #FFF; border-bottom:1px solid #FFF;" height="113">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
 
-<div>
-    To validate you email, go to the following url: {{ URL::to('validation/'.$id.'/'.$token) }}.
-</div>
+            <!--WHITE TEXT AREA-->
+            <td width="131" bgcolor="#FFFFFF" style="border-top:1px solid #FFF; text-align:center;" height="113" valign="middle">
+                <span style="font-size:25px; font-family:Trebuchet MS, Verdana, Arial; color:#2e8a3b;">Welcome!</span>
+            </td>
+
+            <!--GREEN TEXT AREA-->
+            <td background="images/greenback.gif" bgcolor="#45a853" style="border-top:1px solid #FFF; border-bottom:1px solid #FFF; padding-left:15px;" height="113">
+                <span style="color:#FFFFFF; font-size:18px; font-family:Trebuchet MS, Verdana, Arial;">Thank you for registering to our website.</span>
+            </td>
+        </tr>
+
+        <!--DOUBLE BORDERS BOTTOM-->
+        <tr>
+            <td height="3" width="31" style="border-top:1px solid #e4e4e4; border-bottom:1px solid #e4e4e4;">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+            <td height="3" width="131">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+            <td height="3" style="border-top:1px solid #e4e4e4; border-bottom:1px solid #e4e4e4;">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="3">
+                <!--CONTENT STARTS HERE-->
+                <br />
+                <br />
+                <table cellpadding="0" cellspacing="0">
+                    <tr>
+                        <td width="15"><div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+                        </td>
+                        <td width="325" style="padding-right:10px; font-family:Trebuchet MS, Verdana, Arial; font-size:12px;" valign="top">
+                            <span style="font-family:Trebuchet MS, Verdana, Arial; font-size:17px; font-weight:bold;">Welcome!</span>
+                            <br />
+                            <p>You have succesfully signed up for Pilection, only thing to do is confirm your email address, by clicking on the following link:
+
+                            <p>URL::to('validation/'.$id.'/'.$token, 'Confirm your email!')</p>
+
+                            Best Regards,<br/>
+                            Robin Kuiper<p/>
+
+                            Pilection<br/>
+                            <br/>
+
+                            This welcome email was sent because you recently signed up for <a href="http://pilection.eu" title="Pilection.eu">Pilection.eu</a>.
+
+                        </td>
+                        <td style="border-left:1px solid #e4e4e4; padding-left:15px;" valign="top">
+
+                            <!--RIGHT COLUMN SECOND BOX-->
+                            <br />
+                            <table width="100%" cellpadding="0" cellspacing="0" style="border-bottom:1px solid #e4e4e4; font-family:Trebuchet MS, Verdana, Arial; font-size:12px;">
+                                <tr>
+                                    <td>
+                                        <div style="font-family:Trebuchet MS, Verdana, Arial; font-size:17px; font-weight:bold; padding-bottom:10px;">Have Any Questions?</div>
+                                        <p>Don't hesitate to hit the reply button to any of the messages you receive.</p>
+                                        <br />
+                                    </td>
+                                </tr>
+                            </table>
+
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+    <br />
+    <table cellpadding="0" style="border-top:1px solid #e4e4e4; text-align:center; font-family:Trebuchet MS, Verdana, Arial; font-size:12px;" cellspacing="0" width="600">
+        <tr>
+            <td height="2" style="border-bottom:1px solid #e4e4e4;">
+                <div style="line-height: 0px; font-size: 1px; position: absolute;">&nbsp;</div>
+            </td>
+        </tr>
+        <td style="font-family:Trebuchet MS, Verdana, Arial; font-size:12px;">
+            <br />
+            info@pilection.eu<br />
+        </td>
+        </tr>
+    </table>
+</center>
 </body>
 </html>
--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -52,7 +52,7 @@
                 </div>
             </div>
         </div>
-        <div class="row">
+        <!--<div class="row">
             <div class="col-xs-3 col-sm-3 col-md-3">
                 <span class="button-checkbox">
                     <button type="button" class="btn" data-color="info" tabindex="7">I Agree</button>
@@ -62,7 +62,7 @@
             <div class="col-xs-9 col-sm-9 col-md-9">
                 By clicking <strong class="label label-primary">Register</strong>, you agree to the <a href="#" data-toggle="modal" data-target="#t_and_c_m">Terms and Conditions</a> set out by this site, including our Cookie Use.
             </div>
-        </div>
+        </div>-->
 
         <hr class="colorgraph">
         <div class="row">
@@ -100,13 +100,71 @@
                 <h4 class="modal-title" id="myModalLabel">Terms & Conditions</h4>
             </div>
             <div class="modal-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique, itaque, modi, aliquam nostrum at sapiente consequuntur natus odio reiciendis perferendis rem nisi tempore possimus ipsa porro delectus quidem dolorem ad.</p>
+                <p>Om ruzie op onze website te vermijden hebben we regels samengesteld waar men als bezoeker mee <strong>akkoord</strong> moet <strong>gaan</strong>. Wanneer men de regels overtreedt krijgt men een waarschuwing of een ban bij meerdere overtredingen.
+                <hr noshade></P>
+                <h5>1. Gedrag:</h5>
+                <p>Er mogen geen racistische opmerkingen gemaakt worden op Sitemasters.be.<br>
+                    Ook schelden is niet toegelaten.</p>
+                <h5>2. Reclame:</h5>
+                <p>Het plaatsen van reclame is niet gewenst op Sitemasters.be. Enkel als men toestemming krijgt van de webmasters is het toegestaan. Links naar Porno/Warez/Clickgames zijn ten strengste verboden !</p>
+                <h5>3. Geen geflood:</h5>
+                <p>Er mag niet geflood worden op Sitemasters.be. Hiermee bedoelen we geen overbodige of herhalende teksten, geen volledige scripts posten in het forum, ...</p>
+                <h5>4. Kopi&euml;ren van inhoud:</h5>
+                <p>Het kopi&euml;ren van inhoud (tekst, afbeeldingen, lay-out, ...) is verboden zonder toestemming van de beheerder. </p>
+                <h5>5. Taalgebruik:</h5>
+                <p> Schrijf je berichten in het Algemeen Nederlands. Gebruik leestekens waar nodig, geen zelf uitgevonden afkortingen en geen MSN-taal. Hou er rekening mee dat hier Nederlandstalige mensen uit verschillende streken bijeenkomen, en zo ook verschillende dialecten. Het ABN biedt hier dus weeral een ideale middenweg, hou je hier dan ook aan. </p>
+                <h5>6. Scripts:</h5>
+                <p>Het kopi&euml;ren van scripts van een andere website is verboden. Enkel zelf gemaakte scripts mogen gepost worden. Wanneer je een script wil posten kijk je eerst of er al een ongeveer zelfde script is gepost. Dit is om te voorkomen dat er hetzelfde script in komt te staan.</p>
+                <h5>7. Forum:</h5>
+                Bij gebruikmaking van het forum dienen de volgende regels in acht te worden genomen:
+                <ol>
+                    <li><b>voor het plaatsen van een bericht</b><br />
+                        Zorg dat je de volgende twee dingen hebt gedaan voordat je een nieuw bericht plaatst,<br />
+                        het kan	namelijk zijn dat je vraag al beantwoord is:
+                        <ul>
+                            <li>kijk in de <a href="http://www.sitemasters.be/FAQ" target="_blank"><b>categorieën van de FAQ</b></a></li>
+                            <li>gebruik de <a href="http://www.sitemasters.be/forum/zoek" target="_blank"><b>zoekfunctie van het forum</b></a></li>
+                        </ul>
+                    </li>
+
+                    <li><b>bij het plaatsen van een bericht</b><br />
+                        <ul>
+                            <li>geef het onderwerp van een bericht een <u>omschrijvende</u> titel<br /></li>
+                            <li>geef in het bericht een <u>duidelijke omschrijving van het probleem</u> of een <u>concrete foutmelding</u></li>
+                            <li>voorzie het bericht eventueel van <u>codefragmenten</u> (<b>géén</b> lappen text)</li>
+                            <li>vermeld erbij wat het <i>gewenste gedrag</i> van de code is en hoe jouw code hier van afwijkt</li>
+                            <li>plaats het zelfde bericht niet op meerdere fora; als je vind dat er een forum ontbreekt, meld dit dan
+                                aan een medewerker</li>
+                            <li>het forum is een plaats waar mensen elkaar proberen te helpen, start hier dus geen flamewars</li>
+                        </ul>
+                    </li>
+
+                    <li><b>na het plaatsen van een bericht</b><br />
+                        <ul>
+                            <li>plaats geen (directe) reacties op eigen berichten en eigen reacties<br />
+                                wanneer mensen iets bij kunnen dragen aan de discussie of aan de oplossing van een probleem,
+                                is het aan hun om te bepalen of en wanneer ze reageren, niet aan de starter van het bericht</li>
+                            <li>laat weten wanneer een probleem is opgelost, zodat medewerkers het bericht kunnen sluiten</li>
+                        </ul>
+                    </li>
+
+                    <li><b>bij het plaatsen van een reactie</b><br />
+                        <ul>
+                            <li>plaats een reactie als je denkt een <u>zinvolle bijdrage</u> te kunnen leveren aan de discussie</li>
+                            <li>probeer ontopic te blijven - zinloze spam zal door onze bikkelharde medewerkers worden geneutraliseerd :)</li>
+                            <li>probeer oplossingen toe te lichten (bijvoorbeeld door commentaar in code toe te voegen) - het is vaak
+                                interessanter om te zien hoe je tot een oplossing gekomen bent (of waarom je voor die oplossing gekozen hebt)
+                                dan het geven van enkel de oplossing zelf; het toelichten van oplossingen draagt bij aan het <u>begrip</u></li>
+                        </ul>
+                    </li>
+                </ol>
+                <h5>8. Cracken &amp; Hacken:</h5>
+                <p>Het proberen te cracken/hacken op Sitemasters is verboden. Dit zal dan ook zwaar worden gestraft.<br>
+                    <br>
+                    <strong>9. Informatie op Sitemasters:</strong><br>
+                    <br>
+                    Alle content (tutorials, scripts, forumberichten, wormdelen, ...) die op Sitemasters is gepost, is eigendom van Sitemasters en mag dan ook later niet verwijderd worden.</p>
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" data-dismiss="modal">I Agree</button>
